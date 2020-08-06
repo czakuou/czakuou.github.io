d2bf66dc9e77417099798069afa87c0a2e1119c2
czaku <pakata321@gmail.com>
2020-08-06 11:29:54 +0200

    Tip Calc

---
+++ app.js
@@ -0,0 +1,17 @@
+const calculateTip = () => {
+  const billamt = document.getElementById('billamt').value
+  const serviceQual = document.getElementById('serviceQual').value
+  const peopleamt = document.getElementById('peopleamt').value
+
+  if(peopleamt === 0 || billamt === 0) alert(`Please enter values`)
+  if(peopleamt <= 0 || billamt <= 0) alert('Enter correct values')
+
+  const totalTip = (billamt * serviceQual) / peopleamt
+
+  document.getElementById('totalTip').style.display = 'block'
+  document.getElementById('tip').innerHTML = totalTip
+}
+document.getElementById('totalTip').style.display = 'none'
+document.getElementById('each').style.display = 'none'
+
+document.getElementById('calculate').onclick = () => calculateTip()
