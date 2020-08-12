4ec3a22e1d07e32d8ef4a89a0d03af2aaddfa0be
czaku <pakata321@gmail.com>
2020-08-12 14:21:22 +0200

    weather-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import CurrentLocationWeather from './components/CurrentLocationWeather'
+import styles from './components/styles/index.css'
+
+
+function App() {
+    
+  
+
+
+  return (
+    <div>
+      <CurrentLocationWeather />
+    </div>
+  )
+}
+
+export default App;
